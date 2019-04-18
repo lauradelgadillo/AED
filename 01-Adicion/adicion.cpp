e2cbbf1872a458a45d90bc63031c5341bbd8006c
ldelgadillo <49331109+lauradelgadillo@users.noreply.github.com>
2019-04-18 19:09:11 -0300

    Add files via upload

---
+++ 01-Adicion/adicion.cpp
@@ -0,0 +1,19 @@
+/*Adición
+Obtener del usuario dos números y mostrarle la suma.
+Delgadillo, Laura
+20190418
+*/
+
+#include <iostream>
+
+int main()
+{
+	int a, b;
+
+	std::cin >> a;
+	std::cin >> b;
+
+    std::cout << a+b; 
+}
+
+
