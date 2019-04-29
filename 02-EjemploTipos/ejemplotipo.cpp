c63e562afd1455b68d8c26dedf1fcc2d9fff7b92
ldelgadillo <49331109+lauradelgadillo@users.noreply.github.com>
2019-04-28 22:01:09 -0300

    Add files via upload

---
+++ 02-EjemploTipos/ejemplotipo.cpp
@@ -0,0 +1,16 @@
+/*Ejemplo de valores y operaciones de tipo de dato
+Ejemplo double 
+Delgadillo, Laura
+28/04/2019
+*/
+
+#include <iostream>
+
+int main()
+{
+	double a{ 18.578 };
+	double b{ 5.74 };
+    
+	std::cout << a/b; 
+}
+
